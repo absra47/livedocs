45d4f638154bb18be1fe5656e66fc5b4ae74bcc0
absra47 <absragirma630@gmail.com>
2024-08-14 21:40:31 +0300

    Edit Document Title Feature

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -16,7 +16,12 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
   if (!room) redirect("/");
   return (
     <main className="flex  w-full flex-col items-center">
-      <CollaborativeRoom roomId={id} roomMetadata={room.metadata} />
+      <CollaborativeRoom
+        roomId={id}
+        roomMetadata={room.metadata}
+        users={[]}
+        currentUserType={"creator"}
+      />
     </main>
   );
 };
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -1,6 +1,5 @@
 "use server";
 import { nanoid } from "nanoid";
-import { title } from "process";
 import { liveblocks } from "../liveblocks";
 import { revalidatePath } from "next/cache";
 import { parseStringify } from "../utils";
@@ -12,21 +11,60 @@ export const createDocument = async ({
 
   try {
     const metadata = {
-      cratorId: userId,
+      creatorId: userId,
       email,
-      title: " untitled",
+      title: "Untitled",
     };
+
     const usersAccesses: RoomAccesses = {
       [email]: ["room:write"],
     };
+
     const room = await liveblocks.createRoom(roomId, {
       metadata,
       usersAccesses,
       defaultAccesses: ["room:write"],
     });
     revalidatePath("/");
+
     return parseStringify(room);
   } catch (error) {
-    console.log(`Error  happened while creating a room ${error}`);
+    console.log(`Error happened while creating a room: ${error}`);
+  }
+};
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    if (!hasAccess) {
+      throw new Error("You do not have access to this document");
+    }
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`Error happened while getting a room: ${error}`);
+  }
+};
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+
+    revalidatePath(`/documents/${roomId}`);
+
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(`Error happened while updating a room: ${error}`);
   }
 };
